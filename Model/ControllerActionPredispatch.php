9fa93ec5fc014b8d78da8fd28c4100560092f1d5
Prashant Patel <prashantpatel@accorin.com>
2022-05-16 17:07:58 +0530

    Magento 2.4.4 compile issue fixed

--- Model/ControllerActionPredispatch.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace WolfSellers\EnableDisableTfa\Model;
-
-use Magento\TwoFactorAuth\TestFramework\ControllerActionPredispatch as MagentoControllerActionPredispatch;
-
-class ControllerActionPredispatch extends MagentoControllerActionPredispatch
-{
-}
